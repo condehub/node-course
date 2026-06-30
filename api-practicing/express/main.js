9bf0a7e23fff0ee73de79d9d8ab7003a801e58fe
Conde <joao.vbconde@gmail.com>
2026-06-30 11:48:00 -0400

    package and package-lock .json files and main js

---
+++ api-practicing/express/main.js
@@ -0,0 +1,12 @@
+import express from 'express'
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('A api está funcionando')
+})
+
+app.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000');
+});
+
